560697d30c8dcac1cc7dae55adc148a1fbbf0d17
sachin175638 <35227965+sachin175638@users.noreply.github.com>
2018-02-19 18:08:26 +0530

    Delete setup.sh

--- setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-chmod +x tools.sh
-ln -s $HOME/tools/tools.sh /data/data/com.termux/files/usr/bin/tools
-echo "============================"
-echo "setup completed ............"
-echo "============================"
-echo "just execute tools"
